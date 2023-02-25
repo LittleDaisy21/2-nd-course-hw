e96f8449a0ccd56a9e98874b77985ef1e382f531
Mariia <miraclefrommars@gmail.com>
2023-02-25 23:18:27 +0100

    Task 1

---
+++ hw-3.js
@@ -0,0 +1,11 @@
+// Задание 1
+let password = 'пароль';
+let userPassword = String(prompt('Введите пароль'));
+if (userPassword === password) {
+    console.log("Пароль введен верно");
+} else {
+    console.log("Пароль введен неправильно");
+}
+
+// Задание 2
+let c = 3;
