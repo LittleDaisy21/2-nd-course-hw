392f13c460de681dbf1f88cd1dec786c980acfcd
Mariia <miraclefrommars@gmail.com>
2023-02-26 19:33:55 +0100

    Task 2-5

--- hw-3.js
+++ hw-3.js
@@ -8,4 +8,79 @@ if (userPassword === password) {
 }
 
 // Задание 2
-let c = 3;
+let c = 2;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+// Задание 3
+let d = 110;
+let e = 56;
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+// Задание 4
+let a = '2';
+a = 2;
+let b = '3';
+b = 3;
+alert(a + b);
+
+// Задание 5
+let monthNumber = String(prompt('Какой сейчас месяц?'));
+switch (monthNumber) {
+    case '1':
+    console.log('Зима');
+    break;
+    case '2':
+    console.log('Зима');
+    break;
+    case '12':
+    console.log('Зима');
+    break;
+    case '3':
+    console.log('Весна');
+    break;
+    case '4':
+    console.log('Весна');
+    break;
+    case '5':
+    console.log('Весна');
+    break;
+    case '6':
+    console.log('Лето');
+    break;
+    case '7':
+    console.log('Лето');
+    break;
+    case '8':
+    console.log('Лето');
+    break;
+    case '9':
+    console.log('Осень');
+    break;
+    case '10':
+    console.log('Осень');
+    break;
+    case '11':
+    console.log('Осень');
+    break;
+    default: console.log('Ошибка');
+    break;
+}
+
+// Задание 6
+
+
+// Задание 7
+
+
+// Задание 8
+
+
+// Задание 9
