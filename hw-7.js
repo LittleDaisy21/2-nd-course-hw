3c7ad3a999c893996179e5878d8b3b6d5d8e0b69
Mariia <miraclefrommars@gmail.com>
2023-03-19 18:41:03 +0100

    1-4 tasks done

--- hw-7.js
+++ hw-7.js
@@ -19,14 +19,20 @@ animals.forEach((animal) => {
 // Задание 3
 
 let num = 32.58884;
-Math.round
-Math.floor
-Math.ceil 
+console.log(Math.floor(num)); // Округляет аргумент до ближайшего меньшего целого.
+console.log(Math.ceil(num)); // Округляет аргумент до ближайшего большего целого.
+console.log(Math.round(num)); // возвращает число, округлённое к ближайшему целому.
+
 
 
 // Задание 4
 
+let numbers = [52, 53, 49, 77, 21, 32];
+let maxNumber = Math.max(...numbers);
+let minNumber = Math.min(...numbers);
 
+console.log(maxNumber);
+console.log(minNumber);
 
 // Задание 5
 
